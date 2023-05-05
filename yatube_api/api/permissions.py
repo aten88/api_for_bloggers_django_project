cf79b3658be52e29ef83c95d5755903342a3e249
aten88 <virtuoso_ten@mail.ru>
2023-05-05 15:38:00 +0300

    Fixed last mark review ver 3

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,10 +2,10 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthorOrReadOnly(BasePermission):
-    """Custom permission for safety requests"""
+    """Custom permission for safe requests"""
     def has_permission(self, request, view):
         """Allow GET, HEAD, OPTIONS requests or authenticated users."""
-        return request.method
+        return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         """Allow only the author to edit and delete the object."""
